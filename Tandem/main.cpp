b915041acf710876b871a61743f762bbc3e432b5
Andrey Styskin <styskin@77.88.19.206-red.dhcp.yndx.net>
2018-02-08 18:14:25 +0300

    Initial Commit

---
+++ Tandem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tandem
+//
+//  Created by Andrey Styskin on 08.02.18.
+//  Copyright © 2018 Andrey Styskin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
